b63fcab2c19487e7084cea4dc09c4b70a20e7469
Karl Scheel <karl@scheelweb.de>
2021-03-27 15:20:19 +0100

    remove todo comment

--- PauseToggle-For-Multiplayer.js
+++ PauseToggle-For-Multiplayer.js
@@ -1,15 +1,5 @@
 /// <reference path="../openrct2.d.ts" />
 
-/* ToDos:
-[x] Nur als Server ausführen? Nur der braucht die Chatnachrichten ja zu verarbeiten.
-[x] Infos wie auslösenden Spieler anzeigen
-    > geht nicht als park.postMessage.. Das wird nicht angezeigt wenn das Spiel pausiert ist.
-    > dann wohl nur als ChatMessage?
-[x] auf Berechtigung prüfen
-[ ] Button einbauen statt ChatMsg?! Nur ein Client braucht den Button, Host hat ja den normalen Button.
-    > Müsste ich dafür eine eigene network.action definieren und den Server darauf subscriben lassen? Und er Button löst die action aus? :s
-*/
-
 // configuration constants - feel free to change!
 const CNST_TriggerMessages = ["p", "pause"];    // the chat messages that will trigger the pause toggle
 
